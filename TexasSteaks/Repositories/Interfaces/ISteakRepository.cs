0fba94c67d3e2dbf278b77b6f5a906e0d0cc7a5e
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-11-16 22:32:23 -0300

    Implementing the Steak repository.

---
+++ TexasSteaks/Repositories/Interfaces/ISteakRepository.cs
@@ -0,0 +1,11 @@
+﻿using TexasSteaks.Models;
+
+namespace TexasSteaks.Repositories.Interfaces
+{
+    public interface ISteakRepository
+    {
+        IEnumerable<Steak> Steaks { get; }
+        IEnumerable<Steak> FavoritesSteaks { get; }
+        Steak GetSteakById (int id);
+    }
+}
---
+++ TexasSteaks/Repositories/SteakRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TexasSteaks.Context;
+using TexasSteaks.Models;
+using TexasSteaks.Repositories.Interfaces;
+
+namespace TexasSteaks.Repositories
+{
+    public class SteakRepository : ISteakRepository
+    {
+        private readonly AppDbContext _context;
+        public SteakRepository(AppDbContext context) {
+            _context = context;
+        }
+
+        public IEnumerable<Steak> Steaks => _context.Steaks.Include(c => c.Category);
+
+        public IEnumerable<Steak> FavoritesSteaks => _context.Steaks
+            .Where(s => s.IsFavorite)
+            .Include(c => c.Category);
+
+        public Steak GetSteakById(int id)
+        {
+            return _context.Steaks.FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
