90857638c93e55cd3c9fa983a7a97c7b040450c8
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-12-13 22:59:01 -0300

    Creating a ViewComponent to display the number of items in the cart and adjusting the main layout.

---
+++ eShop/Components/ShoppingCartSummary.cs
@@ -0,0 +1,43 @@
+﻿using eShop.Models;
+using eShop.Repositories.Interfaces;
+using eShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly IShoppingCartRepository _shoppingCartRepository;
+
+        public ShoppingCartSummary(IShoppingCartRepository shoppingCartRepository)
+        {
+            _shoppingCartRepository = shoppingCartRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var shoppingCart = _shoppingCartRepository.GetCart();
+
+            // Code snippet for testing the item count in the shopping cart.
+            //var items = new List<ShoppingCartItem>()
+            //{
+            //    new ShoppingCartItem(),
+            //    new ShoppingCartItem()
+            //};
+
+            //ShoppingCart shoppingCart = new ShoppingCart()
+            //{
+            //    ShoppingCartItems = items
+            //};
+            // End of test segment.
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = shoppingCart,
+                ShoppingCartTotal = _shoppingCartRepository.GetShoppingCartTotal(shoppingCart.Id)
+            };
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
