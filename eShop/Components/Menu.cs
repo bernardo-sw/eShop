30ae160330a1936e274aa749042b717b0a8460ee
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-12-16 00:45:50 -0300

    Creating a ViewComponent to display the categories in the Products menu.

---
+++ eShop/Components/Menu.cs
@@ -0,0 +1,21 @@
+﻿using eShop.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Components
+{
+    public class Menu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public Menu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.Categories.OrderBy(c => c.Name);
+            return View(categories);
+        }
+    }
+}
