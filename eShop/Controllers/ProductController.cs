c3a0ef963af736b266c2c880866f4744b81cf116
Bernardo Marambaia <marambaia.bma@gmail.com>
2026-01-02 01:46:17 -0300

    Creating a product search by name.

--- eShop/Controllers/ProductController.cs
+++ eShop/Controllers/ProductController.cs
@@ -50,5 +50,36 @@ namespace eShop.Controllers
             var product = _productRepository.GetProductById(id);
             return View(product);
         }
+
+        public IActionResult Search(string searchString)
+        {
+            IEnumerable<Product> products;
+            CategoryInfoViewModel categoryInfo = new()
+            {
+                Name = "List of Products",
+                Description = "Full list of our products that will expand your creativity (and open your palate).",
+            };
+
+            if (string.IsNullOrEmpty(searchString))
+            {
+                products = _productRepository.Products.OrderBy(p => p.Name);
+            }
+            else
+            {
+                products = _productRepository.Products
+                          .Where(p => p.Name.ToLower().Contains(searchString.ToLower()));
+
+                if (products.Any())
+                    categoryInfo.Description = "Products found that match the searched name.";
+                else
+                    categoryInfo.Description = "No products were found matching the searched name.";
+            }
+
+            return View("~/Views/Product/List.cshtml", new ProductListViewModel
+            {
+                Products = products,
+                CurrentCategory = categoryInfo
+            });
+        }
     }
 }
