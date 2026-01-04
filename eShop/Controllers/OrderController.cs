06da794a84b7bb3d1a3931e880345b59bacb2e6d
Bernardo Marambaia <marambaia.bma@gmail.com>
2026-01-03 21:41:42 -0300

    Create the OrderController and implement the Checkout.

---
+++ eShop/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using eShop.Models;
+using eShop.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IShoppingCartRepository _shoppingCartRepository;
+
+        public OrderController(IOrderRepository orderRepository, IShoppingCartRepository shoppingCartRepository)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCartRepository = shoppingCartRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            return View();
+        }
+    }
+}
