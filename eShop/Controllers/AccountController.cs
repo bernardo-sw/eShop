f5cee26c8582d984254b3e15a1a2476d647097f7
Bernardo Albuquerque <bernardo-sw@outlook.com>
2026-01-06 00:22:21 -0300

    Implementing authentication.

---
+++ eShop/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using eShop.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(loginViewModel);
+
+            var user = await _userManager.FindByNameAsync(loginViewModel.UserName);
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(loginViewModel.ReturnUrl))
+                        return RedirectToAction("Index", "Home");
+
+                    return Redirect(loginViewModel.ReturnUrl);
+                }
+            }
+            ModelState.AddModelError("", "Login failed!");
+            return View(loginViewModel);
+        }
+    }
+}
---
+++ eShop/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eShop.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please provide the name.")]
+        [Display(Name = "User")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Enter the password.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
