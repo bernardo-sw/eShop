e6f07b6742675ee921e0f6cb678cc6a08adc7e66
Bernardo Albuquerque <bernardo-sw@outlook.com>
2026-01-08 21:39:47 -0300

    Creating and configuring the AdminController.

--- eShop/Program.cs
+++ eShop/Program.cs
@@ -53,7 +53,7 @@ namespace eShop
 
             app.MapControllerRoute(
                  name: "areas",
-                 pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
+                 pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                  );
 
             app.MapControllerRoute(
